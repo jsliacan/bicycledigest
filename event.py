e4a7b1605d2ebf30b915840396242b10f544a621
Jakub Sliacan <jsliacan@proton.me>
2025-11-08 15:32:50 +0100

    Add GPS trace and clean up, refactor

--- event.py
+++ event.py
@@ -3,6 +3,7 @@ leidenalg docs:     https://leidenalg.readthedocs.io/en/latest/
 leidenalg repo:     https://github.com/vtraag/leidenalg
 """
 
+import hashlib
 import logging
 import os
 
@@ -15,96 +16,159 @@ import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
 
 
+def make_vertices(excerpt_df: pd.DataFrame) -> dict:
+    """
+    Create a dict of vertices: {i: (t_i, d_i)}_i.
+
+    INPUT:
+
+    excerpt_df:     data frame with lidar excerpt based on a button press (event.excerpt)
+    """
+    logging.info("Constructing vertices/nodes.")
+
+    excerpt_df["vertex"] = list(zip(excerpt_df.time.values, excerpt_df.distance.values))
+    V = {i: excerpt_df["vertex"].iloc[i] for i in range(len(excerpt_df["vertex"]))}
+
+    return V
+
+
+def make_edges(vertices: dict, delta: float, epsilon: int) -> dict:
+    """
+    Create a dict of edges with weights: {(i,j): w_ij}
+
+    INPUT:
+
+    vertices:       dict {vertex_index: (timestamp, latdist)}; ordered latdist readings from the lidar
+    delta:          two latdists this close in distance can have an edge between them
+    epsilon:        two latdists this close in time can have edge between them
+    """
+    logging.info("Constructing edges and weights.")
+
+    n = len(vertices)
+    edges = {}
+
+    if n < 2:
+        logging.error("Not enough vertices in the graph: n = %d", n)
+    logging.info("Constructing edges.")
+
+    for i in range(1, n):
+        ti, di = vertices[i]
+        for j in range(i - 1, 0, -1):
+            tj, dj = vertices[j]
+            diff_d = abs(di - dj)
+            diff_t = abs(ti - tj)
+
+            if diff_d < delta and diff_t < np.timedelta64(epsilon, "ms"):
+                edges[(j, i)] = 1
+            elif diff_d < delta and diff_t > np.timedelta64(epsilon, "ms"):
+                for k in range(j + 1, i):
+                    if (k, i) in edges:
+                        # not sure if need next 2 lines (only consider
+                        # vertices k which are close enough to j -- though
+                        # in practice the first one will be like that)
+                        tk, _ = vertices[k]
+                        if abs(tk - tj) < np.timedelta64(epsilon, "ms"):
+                            edges[(j, i)] = 1
+                            break
+    return edges
+
+
+def make_graph(vertices: dict, delta: float, epsilon: int) -> ig.Graph:
+    """
+    Construct a weighted graph based on the excerpt.
+
+    INPUT:
+
+    vertices:       dict {vertex_index: (timestamp, latdist)}; ordered latdist readings from the lidar
+    delta:          two latdists this close in distance can have an edge between them
+    epsilon:        two latdists this close in time can have edge between them
+    """
+    logging.info("Constructing the graph.")
+    edges = make_edges(vertices, delta, epsilon)
+
+    return ig.Graph(edges.keys(), directed=False)  # only get keys (x,y) edges, ignore weights for now
+
+
+def find_partition(g: ig.Graph, num_iters: int) -> la.ModularityVertexPartition:
+    """
+    Run leidenalg on the graph and save partitions.
+
+    INPUT:
+
+    g:              graph representing our excerpt
+    num_iters:      leidenalg uses randomness in heuristics; higher num_iters
+                    makes it converge more
+    """
+    logging.info("Partitioning the graph.")
+
+    return la.find_partition(g, la.ModularityVertexPartition, n_iterations=num_iters)
+
+
 class Event:
     """
     Class holding information on each 'event' (OC/OT).
     """
 
-    def __init__(self, etype: str, press_start: pd.Timestamp, excerpt: pd.DataFrame, delta=30, epsilon=200) -> None:
+    def __init__(
+        self,
+        etype: str,
+        button_press: pd.DataFrame,
+        lidar_excerpt: pd.DataFrame,
+        gps_excerpt: pd.DataFrame,
+        delta=30,
+        epsilon=200,
+    ) -> None:
         """
         Initialize an instance of Event.
 
-        etype   :       Event type; either 'ot' or 'oc'
-        ps      :       Timestamp of the press start (press time minus duration)
-        excerpt :       DataFrame holding excerpt from lidar df; based on the button press
-        delta   :       Threshold for what we consider close in latdist
-        epsilon :       Threshold for what we consider close in time
+        etype           :   Event type; either 'ot' or 'oc'
+        ps              :   Timestamp of the press start (press time minus duration)
+        lidar_excerpt   :   DataFrame holding excerpt from lidar df; based on the button press
+        delta           :   Threshold for what we consider close in latdist
+        epsilon         :   Threshold for what we consider close in time
         """
-
+        # input
         self.etype = etype
-        self.ps = press_start
-        self.excerpt = excerpt
+        self.ps = button_press["press_start"]
+        self.pe = button_press["time"]
+        self.pd = button_press["duration"]
         self.delta = delta
         self.epsilon = epsilon
+        self.hash = self.get_hash()
 
-        self.vertices = self.make_vertices()
-        self.edges = self.make_edges()
-        self.g = self.make_graph()
-        self.p = self.find_partition()
-        self.part = self.select_part()
+        # processing
+        self.vertices = make_vertices(lidar_excerpt)
+        self.g = make_graph(self.vertices, self.delta, self.epsilon)
+        self.p = find_partition(self.g, 5)  # set num_iters to 5; not something user should be changing much
 
-    def make_vertices(self) -> dict:
+        # output
+        self.part = self.select_part()  # indices of vertices (in self.vertices) corresponding to the OT/OC
+        self.ld_list = [self.vertices[x][1] for x in self.part]  # latdists corresponding to OT/OC
+        self.t_list = [self.vertices[x][0] for x in self.part]  # timestamps corresponding to OT/OC
+        self.gps_trace = self.get_gps_trace(gps_excerpt)  # from OT/OC start till start of button press
+        self.plot_event()
+
+    def get_hash(self) -> str:
         """
-        Create a dict of vertices: {i: (t_i, d_i)}_i.
+        Return event's hash (ID) from a button press timestamp (as string).
         """
+        logging.info("Hashing event's timestamp into an ID.")
 
-        edf = self.excerpt
-        edf["vertex"] = list(zip(edf.time.values, edf.distance.values))
-        V = {i: edf["vertex"].iloc[i] for i in range(len(edf["vertex"]))}
+        h = hashlib.new("sha256")
+        h.update(self.pe.strftime("%Y-%m-%d %H:%M:%S.%f").encode())
 
-        return V
-
-    def make_edges(self) -> dict:
-        """
-        Create a dict of edges with weights: {(i,j): w_ij}
-        """
-        n = len(self.vertices)
-        # print(n, "vertices")
-        # print(self.vertices)
-        edges = {}
-
-        if n < 2:
-            logging.error("Not enough vertices in the graph: n = %d", n)
-        logging.info("Constructing edges.")
-
-        for i in range(1, n):
-            ti, di = self.vertices[i]
-            for j in range(i - 1, 0, -1):
-                tj, dj = self.vertices[j]
-                diff_d = abs(di - dj)
-                diff_t = abs(ti - tj)
-
-                if diff_d < self.delta and diff_t < np.timedelta64(self.epsilon, "ms"):
-                    edges[(j, i)] = 1
-                elif diff_d < self.delta and diff_t > np.timedelta64(self.epsilon, "ms"):
-                    for k in range(j + 1, i):
-                        if (k, i) in edges:
-                            # not sure if need next 2 lines (only consider
-                            # vertices k which are close enough to j -- though
-                            # in practice the first one will be like that)
-                            tk, _ = self.vertices[k]
-                            if abs(tk - tj) < np.timedelta64(self.epsilon, "ms"):
-                                edges[(j, i)] = 1
-                                break
-        return edges
-
-    def make_graph(self) -> ig.Graph:
-        """
-        Construct a weighted graph based on the excerpt.
-        """
-        return ig.Graph(self.edges.keys(), directed=False)  # only get keys (x,y) edges, ignore weights for now
-
-    def find_partition(self) -> la.ModularityVertexPartition:
-        """
-        Run leidenalg on the graph and save partitions.
-        """
-
-        return la.find_partition(self.g, la.ModularityVertexPartition)
+        return h.hexdigest()
 
     def select_part(self) -> list:
         """
-        Return list of vertices that belong in the part corresponding to the vehicle.
+        Return list of vertices in the part corresponding to the vehicle.
+
+        IDEA:
+        Currently just basic: eliminate parts that are too small (at 30+Hz,
+        anything under 4 readings is not a car). Then take the lowest of the
+        big parts.
         """
+        logging.info("Choosing the part (of the partition) corresponding to the event.")
 
         part_candidates = []
         part_avgs = []
@@ -118,18 +182,41 @@ class Event:
 
         return part_candidates[np.argmin(part_avgs)]
 
+    def get_gps_trace(self, gps_df: pd.DataFrame) -> list:
+        """
+        Return a list of (long,lat) values for the duration of the OT/OC.
+
+        TODO:
+        - Extend to OCs
+        """
+
+        if self.etype == "oc":
+            logging.error("GPS trace not implemented.")
+            return []
+
+        logging.info("Constructing GPS trace.")
+
+        trace_end = self.ps  # beginning of the button press
+        trace_start = pd.to_datetime(self.t_list[0], utc=True)  # first timestamp of the OT
+        print(type(trace_start), type(trace_end), type(gps_df["time"].iloc[0]))
+        trace_df = gps_df.loc[(gps_df["time"] > trace_start) & (gps_df["time"] < trace_end)]
+
+        return list(zip(trace_df.latitude.values, trace_df.longitude.values))
+
     def plot_event(self) -> None:
         """
         Plot detected part against excerpt.
         """
 
         plt.clf()
-
+        logging.info("Plotting event and savin figure.")
         verts = list(self.vertices.values())
         unzipped = list(zip(*verts))
+        # plt.xlim(0, 150)
+        plt.ylim(0, 900)
         plt.scatter(unzipped[0], unzipped[1], color="b")
 
         part_verts = list([self.vertices[x] for x in self.part])
         part_unzipped = list(zip(*part_verts))
         plt.scatter(part_unzipped[0], part_unzipped[1], color="r")
-        plt.savefig(os.path.join("out", "detection_" + self.etype + "_" + str(self.ps) + ".png"))
+        plt.savefig(os.path.join("out", "detection_" + self.etype + "_" + self.hash + ".png"))
--- session.py
+++ session.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pandas as pd
 import yaml
 
@@ -26,13 +26,16 @@ class BicycleSession:
 
         self.button_file = self.config["sources"]["button_file"]
         self.lidar_file = self.config["sources"]["lidar_file"]
+        self.gps_file = self.config["sources"]["gps_file"]
 
         self.delta = self.config["detection"]["delta"]["value"]
         self.epsilon = self.config["detection"]["epsilon"]["value"]
 
         self.button_df = self.load_button_file()
-        self.df_ot, self.df_oc = self.decide_otoc()
+        self.gps_df = self.load_gps_file()
         self.lidar_df = self.load_lidar_file()
+        self.df_ot, self.df_oc = self.decide_otoc()
+        self.make_events()
 
     def print_info(self) -> None:
         """
@@ -51,7 +54,7 @@ class BicycleSession:
 
     def load_button_file(self) -> pd.DataFrame:
         """
-        Read in button and return a dataframe.
+        Read in button file and return a dataframe.
         """
 
         filename = os.path.basename(self.button_file)
@@ -60,9 +63,23 @@ class BicycleSession:
 
         df = pd.read_csv(self.button_file, on_bad_lines="skip")
         df = df.loc[df["duration"] > 0.01]
-        df = df.assign(time=pd.to_datetime(df["time"]))  # convert strin UTC time to pd.DateTime
+        df = df.assign(time=pd.to_datetime(df["time"]))  # convert string UTC time to pd.DateTime
         df = df.assign(timedelta=pd.to_timedelta(df["duration"], unit="s"))  # timedelta = duration as pd.Timedelta
         df = df.assign(press_start=df["time"] - df["timedelta"])
+
+        return df
+
+    def load_gps_file(self) -> pd.DataFrame:
+        """
+        Read in GPS file and return a dataframe.
+        """
+
+        filename = os.path.basename(self.gps_file)
+        logging.info("Loading gps CSV file: %s", filename)
+
+        df = pd.read_csv(self.gps_file, on_bad_lines="skip")
+        df = df.assign(time=pd.to_datetime(df["time"]))  # convert string UTC time to pd.DateTime
+        df = df.astype({"latitude": float, "longitude": float})  # make sure values are floats
 
         return df
 
@@ -82,27 +99,47 @@ class BicycleSession:
             logging.info("Removing units from variable names in the header in %s.", filename)
             df.rename(columns={"distance [cm]": "distance"}, inplace=True)
 
-        # get excerpts based on button presses
+        return df
 
-        for ps in self.df_ot["press_start"]:
-            excerpt_start = ps - pd.to_timedelta("3s")
+    def make_events(self) -> None:
+        """
+        Create an event for each button press.
+
+        TODO:
+        - Extend to OCs as well.
+        """
+        logging.info("Making events.")
+
+        ldf = self.lidar_df
+        gpsdf = self.gps_df
+
+        # get excerpts based on button presses
+        for _, bp in self.df_ot.iterrows():
+            ps = bp["press_start"]
+
+            excerpt_start = ps - pd.to_timedelta("3s")  # 3 seconds look-back
             excerpt_end = ps
 
-            excerpt_df = df.loc[(df["time"] > excerpt_start) & (df["time"] < excerpt_end)]
-            # print(excerpt_df)
-            e = Event(etype="ot", press_start=ps, excerpt=excerpt_df, delta=self.delta, epsilon=self.epsilon)
-            # print(e.p)
+            edf_lidar = ldf.loc[(ldf["time"] > excerpt_start) & (ldf["time"] < excerpt_end)]  # excerpt df for lidar
+            edf_gps = gpsdf.loc[(gpsdf["time"] > excerpt_start) & (gpsdf["time"] < excerpt_end)]  # excerpt df for lidar
+            e = Event(
+                etype="ot",
+                button_press=bp,
+                lidar_excerpt=edf_lidar,
+                gps_excerpt=edf_gps,
+                delta=self.delta,
+                epsilon=self.epsilon,
+            )
             self.events.append(e)
 
-            plt.scatter(range(len(excerpt_df["time"])), excerpt_df["distance"])
-            plt.savefig(os.path.join("out", "test" + str(ps) + ".png"))
-            plt.clf()
-
-        return df
+            # plt.scatter(range(len(excerpt_df["time"])), excerpt_df["distance"])
+            # plt.savefig(os.path.join("out", "test" + e.hash + ".png"))
+            # plt.clf()
 
     def decide_otoc(self) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
-        Return dataframes for OT presses and OC presses based on button press lengths.
+        Return dataframes for OT presses and OC presses based on button press
+        lengths.
         """
 
         logging.info("Classifying OTs and OCs from button presses.")
@@ -110,6 +147,6 @@ class BicycleSession:
         OTs = self.button_df.loc[self.button_df["duration"] > self.threshold]
         OCs = self.button_df.loc[self.button_df["duration"] <= self.threshold]
 
-        logging.info("Found %d OTs and %d} OCs.", len(OTs), len(OCs))
+        logging.info("Found %d OTs and %d OCs.", len(OTs), len(OCs))
 
         return OTs, OCs
